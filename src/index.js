4245139d32941820e07b02afb8dd9deb79d3e500
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-11-25 01:51:02 +0800

    Documented

--- src/index.js
+++ src/index.js
@@ -5,24 +5,25 @@ import LessionTwo from "./02RoutingAndNaviagtion/LessonTwo";
 import { BrowserRouter } from "react-router-dom";
 
 /**
- *       @About index.js
+ *      @Fundamentals index.js
  *      + index.js is the starting point of react application. Which means code written
  *        in here will be the first code to executed on the browsers.
  *
  */
 
 /**
- *  @Lession01
- *  Selecting an element with id "root" and assigning this element into constant root variable
- *  And then assiging <App /> component as root variable's child :p
+ *      @Lession01 Properties and State
+ *          - Selecting an element with id "root" and assigning this element into
+ *            constant root variable.
+ *          - And then assiging <App /> component as root variable's child :p
  *
  */
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<LessionTwo />);
 
 /**
- *  @Lession02
- *
+ *  @Lession02 Routing and Navigation
+ *          -
  *
  */
 
@@ -32,3 +33,9 @@ root.render(
     <LessionTwo />
   </BrowserRouter>
 );
+
+/**
+ *  @Lession03
+ *
+ *
+ */
