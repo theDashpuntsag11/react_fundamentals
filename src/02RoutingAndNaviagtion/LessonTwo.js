61c841471dbcbc84c40ca0451398b077347a5c95
theDashpuntsag11 <thedashpuntsag@gmail.com>
2023-01-06 09:35:07 +0800

    vulrunability fixed

--- src/02RoutingAndNaviagtion/LessonTwo.js
+++ src/02RoutingAndNaviagtion/LessonTwo.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./index.css";
 
+/**
+ *    @SPA_SinglePageApplication
+ *      - Single page application means that your app runs on a single html DOM. 
+ *        But it's capable of showing different views to user. 
+ *      - By using this kind of approach you can enhacne the UX a lot.
+ * 
+ *    @ReactRouter 
+ *      - Router is a special tool which actually watches changes in the URL, and then 
+ *        changes whats's visible on the screen based on the URL.
+ * 
+ */
+
+
 function LessionTwo() {
   return <div>LessonTwo</div>;
 }
--- src/index.js
+++ src/index.js
@@ -6,29 +6,33 @@ import LessionTwo from "./02RoutingAndNaviagtion/LessonTwo";
 import { BrowserRouter } from "react-router-dom";
 
 /**
- *      @Suggestion
+ *    @Suggestion
  *      To see how every setions work
  *
  *
- *      @FileOverview
+ *    @FileOverview
  *      + index.js is the starting point of react application. Which means code written
  *        in here will be the first code to executed on the browsers.
  *
  */
 
+// --------------------------------------------------------------------------------------------
+
 /**
  *  @Section01 Properties and State
  *
- *  - Selecting an element with id "root" and assigning this element into
- *    constant root variable.
- *  - And then assiging <App /> component as root variable's child :p
+ *    - Selecting an element with id "root" and assigning this element into
+ *      constant root variable.
+ *    - And then assiging <App /> component as root variable's child :p
  */
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<LessionTwo />);
 
+// --------------------------------------------------------------------------------------------
+
 /**
  *  @Section02 Routing and Navigation
- *          -
+ *
  *
  */
 
@@ -39,8 +43,51 @@ root.render(
   </BrowserRouter>
 );
 
+// --------------------------------------------------------------------------------------------
+
 /**
  *  @Section03
  *
+ */
+
+// --------------------------------------------------------------------------------------------
+
+/**
+ *  @Section04
+ *
  *
  */
+
+// --------------------------------------------------------------------------------------------
+
+/**
+ *  @Section05
+ *
+ *
+ */
+
+// --------------------------------------------------------------------------------------------
+
+/**
+ *  @Section06
+ *
+ *
+ */
+
+// --------------------------------------------------------------------------------------------
+
+/**
+ *  @Section08
+ *
+ *
+ */
+
+// --------------------------------------------------------------------------------------------
+
+/**
+ *  @Section09
+ *
+ *
+ */
+
+// --------------------------------------------------------------------------------------------
