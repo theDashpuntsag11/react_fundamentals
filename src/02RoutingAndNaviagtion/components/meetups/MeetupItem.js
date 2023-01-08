4684105a94ad1bbe1d5eab574e6912cc1fafc0e8
theDashpuntsag11 <thedashpuntsag@gmail.com>
2023-01-08 19:17:08 +0800

    list component finished

---
+++ src/02RoutingAndNaviagtion/components/meetups/MeetupItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./MeetupItem.module.css";
+
+function MeetupItem(props) {
+  return (
+    <li className={styles.item}>
+      <div className={styles.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={styles.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={styles.actions}>
+        <button>to Favorite</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
